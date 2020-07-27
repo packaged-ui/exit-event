137ab150d5225550e32f4144b3ca580c87622e28
Tom Kay <oridan82@gmail.com>
2020-07-27 17:23:48 +0100

    simplify

--- src/index.js
+++ src/index.js
@@ -2,31 +2,24 @@
  * Execute (callback) when the cursor exit the page, or the user changes window focus
  *
  * @param {function} callback
- * @param {Number} [debounceDelay=10000] don't run for this many milliseconds after the last execution. < 0 will only execute once
  */
-export function startExitCallback(callback, debounceDelay = 10000)
+export function startExitCallback(callback)
 {
-  prepare(0);
-
-  function prepare(delay)
-  {
-    document.removeEventListener('mouseout', pop);
-    setTimeout(() => document.addEventListener('mouseout', pop), delay);
-  }
-
-  function pop(e)
+  function _eventHandler(e)
   {
     const from = e.relatedTarget || e.toElement;
-
     if(!from || from.nodeName === 'HTML')
     {
-      document.removeEventListener('mouseout', pop);
-      callback();
-      if(debounceDelay >= 0)
-      {
-        prepare(debounceDelay);
-      }
+      document.removeEventListener('mouseleave', _eventHandler);
+      callback(prepare);
     }
   }
-}
 
+  function prepare()
+  {
+    document.removeEventListener('mouseleave', _eventHandler);
+    document.addEventListener('mouseleave', _eventHandler);
+  }
+
+  prepare();
+}
